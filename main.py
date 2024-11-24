3ce75a66a2adf7574f1430e45dbe95a23d8c50e6
Çınar Çeçen <cecencinarmert@proton.me>
2024-11-24 20:50:52 +0300

    changed some words, also 100% moving off of replit

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ devOptions = False
 def calculate(number):
   list = []
   higher = 2
-  if debug:
-    print("Set highest to 2")
+#  if debug:
+#    print("Set highest to 2")
   tempnumber = number
-  if debug:
-    print("Set tempnumber to number")
-    print("entering loop...")
+#  if debug:
+#    print("Set tempnumber to number")
+#    print("entering loop...")
   while True:
-    if debug:
-      print(" IN LOOP 1 | calling findhighest()")
+#    if debug:
+#      print(" IN LOOP 1 | calling findhighest()")
     temphigher = findhighest(tempnumber, higher)
-    if debug:  
-      print(" IN LOOP 1 | got back from call (in calculate() now)")
-      print(f" IN LOOP 1 | higher is now {higher}")
+#    if debug:  
+#      print(" IN LOOP 1 | got back from call (in calculate() now)")
+#      print(f" IN LOOP 1 | higher is now {higher}")
     tempnumber = tempnumber - temphigher
-    if debug:
-      print("appending to the list")
+#    if debug:
+#      print("appending to the list")
     list.append(temphigher)
     if tempnumber == 1:
       list.append(1)
@@ -85,64 +85,65 @@ def choiceOne():
 
 
 def choiceTwo():
-  firstnum = int(input("First: "))
-  lastnum = int(input("Last: "))
-  for i in range(firstnum, lastnum):
-    list = calculate(i)
-    print(f"{i} | {list}")
-  input("\n\n[CONTINUE]")
-  print("\n\n\n       1   2   4   8   16   32   64   128")
-  for i in range(firstnum, lastnum):
-    list = calculate(i)
-    print(f" {i} |", end='')
-    if 1 in list:
-      print(" [x]", end='')
-    else:
-      print(" [ ]", end='')
-    if 2 in list:
-      print(" [x]", end='')
-    else:
-      print(" [ ]", end='')
-    if 4 in list:
-      print(" [x]", end='')
-    else:
-      print(" [ ]", end='')
-    if 8 in list:
-      print(" [x]", end='')
-    else:
-      print(" [ ]", end='')
-    if 16 in list:
-      print(" [xx]", end='')
-    else:
-      print(" [  ]", end='')
-    if 32 in list:
-      print(" [xx]", end='')
-    else:
-      print(" [  ]", end='')
-    if 64 in list:
-      print(" [xx]", end='')
-    else:
-      print(" [  ]", end='')
-    if 128 in list:
-      print(" [xx]", end='\r')
-    else:
-      print(" [  ]", end='\r')
-    time.sleep(0.05)
+    firstnum = int(input("First: "))
+    lastnum = int(input("Last: "))
+    for i in range(firstnum, lastnum):
+        list = calculate(i)
+        print(f"{i} | {list}")
+    input("\n\n[CONTINUE]")
+    print("\n\n\n       1   2   4   8   16   32   64   128")
+    for i in range(firstnum, lastnum):
+        list = calculate(i)
+        print(f" {i} |", end='')
+        if 1 in list:
+            print(" [x]", end='')
+        else:
+            print(" [ ]", end='')
+        if 2 in list:
+            print(" [x]", end='')
+        else:
+            print(" [ ]", end='')
+        if 4 in list:
+            print(" [x]", end='')
+        else:
+            print(" [ ]", end='')
+        if 8 in list:
+            print(" [x]", end='')
+        else:
+            print(" [ ]", end='')
+        if 16 in list:
+            print(" [xx]", end='')
+        else:
+            print(" [  ]", end='')
+        if 32 in list:
+            print(" [xx]", end='')
+        else:
+            print(" [  ]", end='')
+        if 64 in list:
+            print(" [xx]", end='')
+        else:
+            print(" [  ]", end='')
+        if 128 in list:
+            print(" [xx]", end='\r')
+        else:
+            print(" [  ]", end='\r')
+        time.sleep(0.05)
     
 
 def choiceThree():
-    pass
+    print("@siliconfire on Github")
+    input()
 
 devOptions = False
 printflush = "Welcome!"
 
 while True:
-  print(f"{printflush}\n------------------------------\n\n[1] Enter one number\n[2] Enter a range of numbers\n[3] Credits")
+  print(f"\n\n{printflush}\n------------------------------\n\n[1] Enter one number\n[2] Enter a range of numbers\n[3] Credits")
   if devOptions:
       if debug:
         print("[4] Turn OFF debug mode\n")
       else:
-        print("[4] Turn ON debug mode\n[5] Disable Developer Options")
+        print("[4] Turn ON debug mode\n[5] Disable Hidden Settings")
   else:
     print("\n")
   choice = input("\nChoose one: ")
@@ -162,28 +163,28 @@ while True:
         else:
           debug = True
       else:
-        printflush = "Developer Options is Disabled! Enable it first to enable debug."
+        printflush = "Hidden settings is Disabled! Enable it first to enable debug."
           
   elif choice == "5":
       if devOptions:
           if not debug:
             devOptions = False
-            printflush = "Developer Options Disabled."
+            printflush = "Hidden settings Disabled."
           else:
             printflush = "Turn OFF Debug with [4] first."
       else:
-        printflush = "Developer Options is already Disabled!"
+        printflush = "Hidden settings is already Disabled!"
           
   elif choice in Keywords:
       if not devOptions:
         devOptions = True
         printflush = "New Options just dropped!"
         os.system("clear")
-        print("Developer options Enabled!\n\nYou have just enabled Developer Mode, which will probably do nothing except for allowing you to enable debug mode. This will be changed in a further update, i have other things to do right now. \n\nTo disable Developer Options, select [5] at the main menu.\n\n")
+        print("Hidden settings Enabled!\n\nYou have just enabled Hidden settings, which will probably do nothing except for allowing you to enable debug mode. This will be changed in a further update, i have other things to do right now. \n\nTo disable Hidden setings, select [5] at the main menu.\n\n")
         input("[CONTINUE]")
 
       else:
-        printflush = "Developer Options is already Enabled!"
+        printflush = "Hidden settings is already Enabled!"
   else:
     printflush = "Invalid input (ValueError)"
     
